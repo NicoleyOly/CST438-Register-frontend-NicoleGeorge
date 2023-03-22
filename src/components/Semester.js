739b3dcf95217e7c2a78801d9b82065d3acc2a74
Nicole <nigeorge@csumb.edu>
2023-03-21 22:29:41 -0700

    implement feature add student

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import Radio from '@mui/material/Radio';
 import {DataGrid} from '@mui/x-data-grid';
 import {SEMESTER_LIST} from '../constants.js'
+import StudentList from './StudentList.js';
 
 // user selects from a list of  (year, semester) values
 class Semester extends Component {
@@ -40,17 +41,47 @@ class Semester extends Component {
         )
       },
       { field: 'name', headerName: 'Semester', width: 200 }
-      ];       
+      ]
+      //const icolumn = [
+        //{
+         // field: 'id',
+          //headerName: 'Name',
+          //width: 200,
+          //rednerCel: (params) => (
+           // <div>
+             // value={params.row.id}
+              //color="default"
+              //size="small"
+             // {StudentList[params.row.id].name}
+
+            //</div>
+          //)
+        //},
+        //{ field: 'name2', headerName: 'Name', width: 200 }
+      //]
+      ;       
        
     return (
        <div>
          <AppBar position="static" color="default">
             <Toolbar>
                <Typography variant="h6" color="inherit">
-                  Schedule - select a term
+                  Schedule - Select a Term
                </Typography>
             </Toolbar>
          </AppBar>
+
+
+         <AppBar position="static" color="default">
+          <Toolbar> 
+            <Button component={Link}
+            to={{pathname:'/addStudent', }} variant="outlined" color="primary" style={{margin: 10}}>
+              Add Student
+            </Button>
+          </Toolbar>
+         </AppBar>
+
+
          <div align="left" >
               <div style={{ height: 400, width: '100%', align:"left"   }}>
                 <DataGrid   rows={SEMESTER_LIST} columns={icolumns} />
---
+++ src/components/StudentList.js
@@ -0,0 +1,174 @@
+import React, { Component } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Cookies from 'js-cookie';
+import {SERVER_URL} from '../constants.js'
+import Grid from '@mui/material/Grid';
+import {DataGrid} from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import AddCourse from './AddCourse';
+import AddStudent from './AddStudent';
+
+class StudentList extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { students: [] }; //empty students right now
+    } 
+    
+    componentDidMount() {
+      this.fetchStudents();
+    }
+    
+    fetchStudents = () => {
+      console.log("StudentList.fetchStudents");
+      const token = Cookies.get('XSRF-TOKEN');
+      
+      fetch(`${SERVER_URL}/addStudent?studentEmail=${this.props.location.email}&studentName=${this.props.location.name}`, 
+        {  
+          method: 'GET', 
+          headers: { 'X-XSRF-TOKEN': token }
+        } )
+      .then((response) => {
+        console.log("FETCH RESP:"+response);
+        return response.json();}) 
+      .then((responseData) => { 
+        // do a sanity check on response
+        if (Array.isArray(responseData.courses)) {
+          this.setState({ 
+            courses: responseData.courses,
+          });
+        } else {
+          toast.error("Fetch failed.", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+        }        
+      })
+      .catch(err => {
+        toast.error("Fetch failed.", {
+            position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error(err); 
+      })
+    }
+  
+    // Drop Course 
+   // onDelClick = (id) => {
+    //  if (window.confirm('Are you sure you want to drop the course?')) {
+    //  const token = Cookies.get('XSRF-TOKEN');
+        
+    //    fetch(`${SERVER_URL}/schedule/${id}`,
+      //    {
+       //     method: 'DELETE',
+        //    headers: { 'X-XSRF-TOKEN': token }
+         // })
+      //.then(res => {
+        //  if (res.ok) {
+          //  toast.success("Course successfully dropped", {
+            //    position: toast.POSITION.BOTTOM_LEFT
+           // });
+        //    this.fetchCourses();
+          //} else {
+           // toast.error("Course drop failed", {
+            //    position: toast.POSITION.BOTTOM_LEFT
+            //});
+           // console.error('Delete http status =' + res.status);
+     // }})
+       // .catch(err => {
+    // toast.error("Course drop failed", {
+      //          position: toast.POSITION.BOTTOM_LEFT
+        //  });
+          //console.error(err);
+       // }) 
+// } 
+  //  }
+  
+    // Add student
+    addStudent = (student) => {
+      const token = Cookies.get('XSRF-TOKEN');
+   
+      fetch(`${SERVER_URL}/addStudent?studentEmail=${this.props.location.email}&studentName=${this.props.location.name}`,
+        { 
+          method: 'POST', 
+          headers: { 'Content-Type': 'application/json',
+                     'X-XSRF-TOKEN': token  }, 
+          body: JSON.stringify(student)
+        })
+      .then(res => {
+          if (res.ok) {
+            toast.success("Student successfully added", {
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+            this.fetchStudents();
+          } else {
+            toast.error("Error when adding", {
+                position: toast.POSITION.BOTTOM_LEFT
+            });
+            console.error('Post http status =' + res.status);
+          }})
+      .catch(err => {
+        toast.error("Error when adding", {
+              position: toast.POSITION.BOTTOM_LEFT
+          });
+          console.error(err);
+      })
+    } 
+  
+    render() {
+       const columns = [
+        { field: 'studentName', headerName: 'Name', width: 400 },
+        { field: 'studentEmail', headerName: 'Email', width: 125 },
+        {
+          field: 'id',
+          headerName: '  ',
+          sortable: false,
+          width: 200,
+          renderCell: (params) => (
+              <Button
+                variant="contained"
+                color="secondary"
+                size="small"
+                style={{ marginLeft: 16 }} 
+                onClick={()=>{this.onDelClick(params.value)}}
+              >
+                Delete Student
+              </Button>
+          )
+        }
+        ];
+    
+    return(
+        <div>
+            <AppBar position="static" color="default">
+              <Toolbar>
+                 <Typography variant="h6" color="inherit">
+                    { 'Student ' + this.props.studentName + ' ' +this.props.studentEmail }
+                  </Typography>
+              </Toolbar>
+            </AppBar>
+            <div className="App">
+              <div style={{width:'100%'}}>
+                  For DEBUG:  display state.
+                  {JSON.stringify(this.state)}
+              </div>
+              <Grid container>
+                <Grid item>
+                  <ButtonGroup>
+                    <AddStudent addStudent={this.addStudent}  />
+                  </ButtonGroup>
+                </Grid>
+              </Grid>
+              <div style={{ height: 400, width: '100%' }}>
+                <DataGrid rows={this.state.courses} columns={columns} />
+              </div>
+              <ToastContainer autoClose={1500} />   
+            </div>
+        </div>
+        ); 
+    }
+  }
+  
+  export default StudentList;
